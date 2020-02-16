351ca3bb6776ba557430c0d5e4d5cb142776748a
Joe Steccato <joseph.p.steccato@gmail.com>
2020-02-16 16:04:38 -0500

    add props for svgs

--- src/svgs/email.js
+++ src/svgs/email.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Email = (props) => (
-	<a style={{width: `2.1rem`, height: `auto`}} href="mailto:joe@joesteccato.com">
+	<a href="mailto:joe@joesteccato.com"
+		style={{
+			width: props.width, 
+			height: `auto`
+		}}>
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 			<title>Email Joe Steccato</title>
 			<path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"/>
